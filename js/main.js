7ea9ed3923483b6812f142b67372c85c4b74128a
Brian D. Hanna <brian.hanna@aarcorp.com>
2017-04-27 23:28:33 -0500

    refactor main game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
 import Deck from './deck';
 import Hand from './hand';
+import Helper from './helper';
 
 const deck = new Deck();
-const hands = [
+let hands = [
   new Hand('player'),
   new Hand('computer')
 ];
 
-const play = function() {
+let war = false;
+let pool = [];
+let playerDeck = document.querySelector('.player .deck');
+
+const round = function() {
+  let plays = play(war);
+  show(plays);
+  let result = resolve(plays);
+  let winner = result.length > 1 ? null : result[0];
+  war = !winner;
+  if (winner) {
+    console.log(winner.player);
+    reward(winner.player);
+  }
+  count();
+}
+
+const play = function(war) {
+  if (war) wager();
   let plays = hands.map(hand => {
     return {
       player: hand.name,
       card: hand.play()
     }
   });
+  plays.forEach(play => { pool.push(play.card); });
   return(plays);
 }
 
@@ -33,37 +53,57 @@ const resolve = function(plays) {
   }, []);
 }
 
+const wager = function() {
+  hands.forEach(hand => {
+    pool.push(hand.play());
+  });
+}
+
+const reward = function(winner) {
+  Helper.shuffler(pool);
+  hands.forEach(hand => {
+    if (hand.name === winner) {
+      while (pool.length > 0) {
+        hand.take(pool.shift());
+      }
+    }
+  })
+}
+
 const count = function() {
   hands.forEach(hand => {
     let elem = document.querySelector(`.${hand.name} .count`);
+    if (hand.cards.length === 0) loser(hand.name);
     elem.innerHTML = hand.cards.length.toString();
   });
 }
 
-const victor = function(p, c) {
-  if (p.value !== c.value) {
-    let winner = p.value > c.value ? 'Player' : 'Computer';
-    console.log(`${winner} wins!`);
-    hands[winner.toLowerCase()].push(...shuffle(pool));
-    pool = [];
-    count();
-  } else {
-    war = true;
-    console.log('War!');
-  }
+const loser = function(player) {
+  let remove = hands.reduce((acc, hand, index) => {
+    return hand.cards.length === 0 ? index : acc;
+  }, 0);
+  hands.splice(remove, 1);
+  victor();
 }
 
-let war = false;
-let pool = [];
-let playerDeck = document.querySelector('.player .deck');
+const victor = function() {
+  if (hands.length === 1) {
+    clearInterval(intervalId);
+    let winner = hands.shift();
+    console.log(winner.name, 'wins!');
+  }
+}
 
 deck.shuffle();
 deck.deal(hands);
 
 count();
 
-playerDeck.addEventListener('click', evt => {
-  let plays = play();
-  let winner = resolve(plays);
-  show(plays);
-});
+let intervalId;
+playerDeck.addEventListener('click', round);
+window.addEventListener('keyup', evt => {
+  if (evt.keyCode === 32) {
+    if (intervalId) clearInterval(intervalId);
+    intervalId = setInterval(round, 250);
+  }
+})
